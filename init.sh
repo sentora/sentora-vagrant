6998b6b72838697982afe5bf22bc3f930301bd0b
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-01-29 13:45:14 +0000

    add basic init - for basic useage : getting sentora-core and sentora-installer. ubuntu32 use provision ubuntu-x32

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Basic default setup of sentora-cora and sentora-installer/preconf"
+
+cd ./Core && git clone git@github.com:sentora/sentora-core.git && git clone git@github.com:sentora/sentora-installers.git && vagrant up
